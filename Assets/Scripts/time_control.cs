6d331ad867342e04b9796e3a2c6e46d15d70ae1c
oars44 <54598471+oars44@users.noreply.github.com>
2020-02-16 12:47:46 -0800

    asadasf

---
+++ Assets/Scripts/time_control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time_control : MonoBehaviour
+{
+    public Text timer;
+    public float start_time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer.text = start_time.ToString("f0");
+        start_time -= Time.deltaTime;
+    }
+}
